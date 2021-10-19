ae14b85a388e87e963df7c91f552056907b5df18
HarlandLohora <harland@lohora.com>
2021-10-18 21:34:04 -0300

    :construction:

---
+++ src/Form/Step/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+const Step = ({ data, step }) => {
+  console.log(data);
+  const { inputs, buttonText, onSubmit } = data;
+
+  return (
+    <Box
+      component="form"
+      autocomplete="off"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+      onSubmit={onSubmit}
+    >
+      {inputs.map((input, i) => {
+        const { label, type, value, valid, onChange, helperText, validator } =
+          input;
+        return (
+          <TextField
+            key={i}
+            label={label}
+            variant="outlined"
+            fullWidth
+            margin="dense"
+            type={type}
+            error={valid === false}
+            helperText={valid === false && helperText}
+            value={value}
+            onChange={(e) => onChange(e, i, step, validator)}
+          />
+        );
+      })}
+
+      <Button variant="contained" type="submit">
+        {buttonText}
+      </Button>
+    </Box>
+  );
+};
+
+export default Step;
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import { LogoSpace, FormSpace, Img } from "./styles";
 import DatosUsuario from "./DatosUsuario";
@@ -6,9 +6,114 @@ import DatosPersonales from "./DatosPersonales";
 import DatosEntrega from "./DatosEntrega";
 import Complete from "./Complete";
 import Stepper from "../Stepper";
+import Step from "./Step";
+
+//Validaciones
+import { validarEmail, validarPassword } from "./DatosUsuario/validaciones";
 
 const Form = () => {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(0);
+  const [pasos, setPasos] = useState({});
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let newStep = step + 1;
+    setStep(newStep);
+    console.log("newStep", newStep);
+    console.log(step);
+  };
+
+  const handleChange = (element, position, currentStep, validator) => {
+    const value = element.target.value;
+    const valid = validator(value);
+    console.log(value);
+    console.log(valid);
+    console.log("currentStep", currentStep);
+    console.log("position", position);
+    console.log("validator", validator);
+
+    pasos[0].inputs[0].label = "Nombre";
+
+    setPasos(pasos);
+
+    console.log(pasos);
+  };
+
+  const stepsFlow = {
+    0: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperTex: "Ingresa un correo electrónico válido.",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperTex:
+            "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20.",
+          validator: validarPassword,
+        },
+      ],
+      buttonText: "Siguiente",
+      onSubmit,
+    },
+    1: {
+      inputs: [
+        {
+          label: "Correo electrónico",
+          type: "email",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperTex: "Ingresa un correo electrónico válido.",
+          validator: validarEmail,
+        },
+        {
+          label: "Contraseña",
+          type: "password",
+          value: "",
+          valid: null,
+          onChange: handleChange,
+          helperTex:
+            "Ingresa una contraseña válida, Al menos 8 caracteres y máximo 20.",
+          validator: validarPassword,
+        },
+      ],
+      buttonText: "Siguiente",
+      onSubmit,
+    },
+  };
+
+  useEffect(() => {
+    console.log("UseEffect");
+    setPasos(stepsFlow);
+  }, []);
+
+  useEffect(() => {
+    console.log(pasos);
+  }, [pasos]);
+
+  useEffect(() => {
+    console.log("Se ha actualizado el step: ", step);
+  }, [step]);
+
+  // useEffect(async () => {
+  //   try {
+  //     const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const posts = await data.json();
+  //     console.log(posts);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // });
 
   //step = 0 --> <DatosUsuario />
   //step = 1 --> <DatosPersonales />
@@ -27,6 +132,8 @@ const Form = () => {
     3: <Complete />,
   };
 
+  console.log("FORM COMPONENT");
+
   return (
     <Box
       sx={{
@@ -41,10 +148,8 @@ const Form = () => {
       </LogoSpace>
       <FormSpace>
         {step < 3 && <Stepper step={step} />}
-        {/* <DatosUsuario />
-        <DatosPersonales />
-        <DatosEntrega /> */}
-        {steps[step]}
+        {/* {steps[step]} */}
+        <Step data={pasos[step]} step={step} />
       </FormSpace>
     </Box>
   );
